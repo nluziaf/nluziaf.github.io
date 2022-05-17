f693c228a5b089359ff5c692435f8be0d889caf5
NLuziaf <76501748+nluziaf@users.noreply.github.com>
2022-05-17 13:42:39 +0700

    Using 1 js file now

--- script.js
+++ script.js
@@ -9,3 +9,14 @@ function typewriter() {
     setTimeout(typewriter, speed);
   }
 }
+
+window.onscroll = function() {
+  const header = document.querySelector('header');
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+      header.classList.add('nav')
+  } else {
+      header.classList.remove('nav')
+  }
+}
